a2cd40c14d5fdce57e14e34d16de75f86df8f334
ValerioCarbone <valeriocarbone10@gmail.com>
2024-04-10 14:23:56 +0200

    Add - Calculator class + create CalculatorTest

--- src/main/java/org/experis/calculator/Calculator.java
+++ src/main/java/org/experis/calculator/Calculator.java
@@ -1,2 +1,24 @@
-package org.experis.calculator;public class Calculator {
+package org.experis.calculator;
+
+public class Calculator {
+
+    // METODI
+
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+
+    public float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
 }
