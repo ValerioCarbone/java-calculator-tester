c96b8bbd254206263b73cbfcde673ad3be375b97
ValerioCarbone <valeriocarbone10@gmail.com>
2024-04-10 16:00:58 +0200

    Add - Tests in CalculatorTest

--- src/main/java/org/experis/calculator/Calculator.java
+++ src/main/java/org/experis/calculator/Calculator.java
@@ -4,21 +4,49 @@ public class Calculator {
 
     // METODI
 
-    public float add(float num1, float num2) {
-        return num1 + num2;
+    public float add(float num1, float num2) throws IllegalArgumentException {
+
+        float result = num1 + num2;
+
+        if (Float.isNaN(num1) || Float.isNaN(num2) || Float.isNaN(result)) {
+            throw new IllegalArgumentException("Input not valid or operation is not valid");
+        }
+
+        return result;
     }
 
 
     public float subtract(float num1, float num2) {
-        return num1 - num2;
+
+        float result = num1 - num2;
+
+        if (Float.isNaN(num1) || Float.isNaN(num2) || Float.isNaN(result)) {
+            throw new IllegalArgumentException("Input not valid or operation is not valid");
+        }
+
+        return result;
     }
 
 
     public float divide(float num1, float num2) {
-        return num1 / num2;
+
+        float result = num1 / num2;
+
+        if (Float.isNaN(num1) || Float.isNaN(num2) || Float.isNaN(result)) {
+            throw new IllegalArgumentException("Input not valid or operation is not valid");
+        }
+
+        return result;
     }
 
     public float multiply(float num1, float num2) {
-        return num1 * num2;
+
+        float result = num1 * num2;
+
+        if (Float.isNaN(num1) || Float.isNaN(num2) || Float.isNaN(result)) {
+            throw new IllegalArgumentException("Input not valid or operation is not valid");
+        }
+
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,51 @@
+import org.experis.calculator.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class CalculatorTest {
+
+    Calculator calc;
+
+    @BeforeEach
+    void init() {
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void notifyTest() {
+        System.out.println("Test done!");
+    }
+
+    @Test
+    @DisplayName("Testing add with valid input")
+    void addTesting() {
+        System.out.println("Test Add");
+        assertEquals(24.00F, calc.add(13, 11));
+    }
+
+    @Test
+    @DisplayName("Testing subtract with valid input")
+    void subtractTesting() {
+        System.out.println("Test Subtract");
+        assertEquals(4.10F, calc.subtract(13.50F, 9.40F), 0.0001);
+    }
+
+    @Test
+    @DisplayName("Testing divide with valid input")
+    void divideTesting() {
+        System.out.println("Test Divide");
+        assertEquals(1.43F, calc.divide(13.50F, 9.40F), 0.01);
+    }
+
+    @Test
+    @DisplayName("Testing multiply with valid input")
+    void multiplyTesting() {
+        System.out.println("Test Multiply");
+        assertEquals(126.9F, calc.multiply(13.50F, 9.40F), 0.01);
+    }
 }
